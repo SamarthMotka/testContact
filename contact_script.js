f567098c857137a8bf812f4e4e906e7ee9e2f656
tejdoshi2000 <68646822+tejdoshi2000@users.noreply.github.com>
2023-12-21 13:21:40 +0530

    Google app script web app made and URL pasted

--- contact_script.js
+++ contact_script.js
@@ -9,3 +9,15 @@ function validateForm() {
       return false;
     }
   }
+
+
+  
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyHuAKnTxHDc11AkCj3DLdsIhJwYQ-GQRRLjXlvnS9daJeuXSX6W7-xmDa1BgzgbsaH/exec'
+  const form = document.forms['Contact Us']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
